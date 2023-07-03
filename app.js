1e8074245c408bf1b6656200b0b49acdde46a3eb
THIÊN BẢO <thienbao@MacBook-Air-cua-THIEN.local>
2023-07-03 20:28:02 +0700

    first add file

--- app.js
+++ app.js
@@ -1,26 +1,44 @@
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const cors = require('cors');
-const indexRouter = require('./routes/index');
-const mongoose = require('mongoose');
-require('dotenv/config');
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const cors = require("cors");
+const indexRouter = require("./routes/index");
+const mongoose = require("mongoose");
+require("dotenv/config");
+const { sendResponse, AppError } = require("./helpers/utils.js");
 
 const app = express();
 
 app.use(cors());
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Connect to MONGODB
 mongoose.connect(process.env.MONGO_URI, () => {
-	console.log('Connected to Database!');
+  console.log("Connected to Database!");
 });
 
-app.use('/', indexRouter);
+app.use("/", indexRouter);
+
+// catch 404 and forard to error handler
+app.use((req, res, next) => {
+  const err = new AppError(404, "Not Found", "Bad Request");
+  next(err);
+});
+/* Initialize Error Handling */
+app.use((err, req, res, next) => {
+  return sendResponse(
+    res,
+    err.statusCode ? err.statusCode : 500,
+    false,
+    null,
+    { message: err.message },
+    err.isOperational ? err.errorType : "Internal Server Error"
+  );
+});
 
 module.exports = app;
--- controllers/car.controller.js
+++ controllers/car.controller.js
@@ -1,37 +1,116 @@
-const mongoose = require('mongoose');
-const Car = require('../models/Car');
+const mongoose = require("mongoose");
+const Car = require("../models/Car");
+const { sendResponse, AppError } = require("../helpers/utils.js");
+
 const carController = {};
 
 carController.createCar = async (req, res, next) => {
-	try {
-		// YOUR CODE HERE
-	} catch (err) {
-		// YOUR CODE HERE
-	}
+  try {
+    // input validation
+    let { make, model, release_date, transmission_type, size, style, price } =
+      req.body;
+    // handle  error
+    if (
+      !make ||
+      !model ||
+      !release_date ||
+      !transmission_type ||
+      !size ||
+      !style ||
+      !price
+    ) {
+      throw new AppError(402, "Bad Request", "Missing require data");
+    }
+    let carNew = await Car.create({
+      make: make,
+      model: model,
+      release_date: release_date,
+      transmission_type: transmission_type,
+      size: size,
+      style: style,
+      price: price,
+    });
+    sendResponse(res, 200, true, { carNew }, null, "create car successfully!");
+  } catch (err) {
+    next(err);
+  }
 };
 
 carController.getCars = async (req, res, next) => {
-	try {
-		// YOUR CODE HERE
-	} catch (err) {
-		// YOUR CODE HERE
-	}
+  //input validation
+  const allowedFilter = ["page", "limit"];
+  try {
+    let { page, limit, ...filterQuery } = req.query;
+    page = parseInt(page) || 1;
+    limit = parseInt(limit) || 10;
+
+    //allow title,limit and page query string only
+    const filterKeys = Object.keys(filterQuery);
+
+    filterKeys.forEach((key) => {
+      if (!allowedFilter.includes(key)) {
+        throw new AppError(402, "Bad Request", "Found list of Cars error");
+      }
+      if (!filterQuery[key]) delete filterQuery[key];
+    });
+    //processing logic
+    //Number of items skip for selection
+    let offset = limit * (page - 1);
+    //mongoose query
+    let allCars = await Car.find();
+    allCars = allCars.filter((item) => item.isDeleted === false);
+    const totalPages = parseInt(allCars.length / limit);
+    //then select number of result by offset
+    allCars = allCars.slice(offset, offset + limit);
+    sendResponse(
+      res,
+      200,
+      true,
+      { cars: allCars, page, total: totalPages },
+      null,
+      "Found list of cars success"
+    );
+  } catch (err) {
+    next(err);
+  }
 };
 
 carController.editCar = async (req, res, next) => {
-	try {
-		// YOUR CODE HERE
-	} catch (err) {
-		// YOUR CODE HERE
-	}
+  try {
+    let { id } = req.params;
+    let updates = req.body;
+    let updatedCar = await Car.findByIdAndUpdate(
+      id,
+      { ...updates },
+      { new: true }
+    ); //returns the document
+    console.log(updatedCar, "test");
+    sendResponse(
+      res,
+      200,
+      true,
+      { car: updatedCar },
+      null,
+      "Update Car Successfully!"
+    );
+  } catch (err) {
+    next(err);
+  }
 };
 
 carController.deleteCar = async (req, res, next) => {
-	try {
-		// YOUR CODE HERE
-	} catch (err) {
-		// YOUR CODE HERE
-	}
+  const { id } = req.params;
+  try {
+    let carIsDeleted = await Car.findByIdAndUpdate(
+      id,
+      { isDeleted: true },
+      { new: true }
+    );
+
+    sendResponse(res, 200, true, {}, null, "Delete Car Successfully!");
+  } catch (err) {
+    next(err);
+  }
 };
 
 module.exports = carController;
